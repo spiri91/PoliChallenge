f675c2a5ddc363654637db9144f9f8f23737983c
spiri91 <spataru.ionut91@yahoo.com>
2018-03-19 22:37:44 +0200

    progress in game screen

--- PoliChallenge/Site/Game/game.js
+++ PoliChallenge/Site/Game/game.js
@@ -1,5 +1,9 @@
 ﻿/// <reference path="../../output/myscripts/app.js" />
 "use strict";
+var gameState = {
+    inProgress: false,
+    score: 0
+};
 
 (function (geo, geolib, _, storage, dealer) {
     let elements = {
@@ -11,6 +15,7 @@
     let questions = [];
     let failedQuestions = 0;
     let score = 0;
+    let inPlay = false;
 
     let objective = {
         latitude: 44.434543,
@@ -23,15 +28,51 @@
 
     function intervaledFunction() {
         geo.get().then((result) => {
+            if (gameState.inProgress) return;
+
+            checkIfAllPlacesHaveBeenVisited();
+
             let distance = getDistance();
             showDistance(distance);
-            checkDistance(distance);
+
+            if (checkDistance(distance))
+                startGameOnPlace();
+            else
+                showTipForNextPlace();
+
             showScore();
         });
     }
 
+    function showTipForNextPlace() {
+
+    }
+
+    function checkIfAllPlacesHaveBeenVisited() {
+
+    }
+
+    function startGameOnPlace() {
+        let place = getNextPlaceAndRemoveIt();
+        let questionsForPlace = getQuestionsForPlace(place);
+
+        gamePlay.start(questionsForPlace);
+    }
+
+    function getQuestionsForPlace(place) {
+        let questionsForPlace = questions.filter((x) => x.for == place.key);
+
+        return questionsForPlace;
+    }
+
+    function getNextPlaceAndRemoveIt() {
+        let place = places.shift();
+
+        return place;
+    }
+
     function showScore() {
-        _.setTextOf(elements.score, score);
+        _.setTextOf(elements.score, gameState.score);
     }
 
     function checkDistance(distance) {
@@ -64,3 +105,66 @@
 
     init();
 })(geo, geolib, _, storage, dealer);
+
+
+var gamePlay = (function (dealer) {
+    let wrongAnsweredQuestions = 0;
+    let _questions = [];
+
+    let currentQuestion = {};
+
+    function start(questions) {
+        gameState.inProgress = true;
+        wrongAnsweredQuestions = 0;
+        _questions = questions;
+        moveNext();
+    }
+
+    function moveNext() {
+        if (wrongAnsweredQuestions == 2 || _questions.length == 0)
+            stopGame();
+
+
+        currentQuestion = _questions.shift();
+        showQuestion();
+    }
+
+    function stopGame() {
+        gameState.inProgress = false;
+    }
+
+    function showQuestion() {
+        let cr = currentQuestion;
+        let answers = [cr.answer1, cr.answer2, cr.answer3, cr.correctAnswer];
+
+        showCard(cr.statement, dealer.shuffle(answers));
+        bindEvents();
+    }
+
+    function showCard(statement, answers) {
+        debugger;
+    }
+
+    function bindEvents() {
+
+    }
+
+    function checkAnswear(e) {
+        if (answerIsCorrect(e))
+            gameState.score += 10;
+        else
+            wrongAnsweredQuestions++;
+
+        moveNext();
+    }
+
+    function answerIsCorrect(e) {
+
+    }
+
+    return {
+        start: start
+    }
+})(dealer);
+
+// TODO extract constants object in separate file
